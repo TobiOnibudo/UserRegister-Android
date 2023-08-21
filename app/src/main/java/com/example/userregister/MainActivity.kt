b5958bde3c76aee9e499b13df4ec68c5dde531fd
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-08-21 09:59:05 -0300

    Retrieving data from database completed

--- app/src/main/java/com/example/userregister/MainActivity.kt
+++ app/src/main/java/com/example/userregister/MainActivity.kt
@@ -1,18 +1,58 @@
 package com.example.userregister
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.userregister.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mainBinding : ActivityMainBinding
+
+    val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+    val myReference : DatabaseReference  = database.reference.child("MyUsers")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         val view = mainBinding.root
         setContentView(view)
 
+        mainBinding.floatingActionButton.setOnClickListener {
+            val intent = Intent(this,AddUserActivity::class.java)
+            startActivity(intent)
+        }
+        retrieveDataFromDatabase()
+
+    }
+
+    private fun retrieveDataFromDatabase(){
+        myReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+               for( eachUser in snapshot.children)
+               {
+                   val user = eachUser.getValue(Users::class.java)
+
+                   if (user != null)
+                   {
+                        println("userId: ${user.userId}")
+                        println("userName: ${user.userName}")
+                        println("userAge: ${user.userAge}")
+                        println("userEmail: ${user.userEmail}")
+                       println("*********************************")
+                   }
+               }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
---
+++ app/src/main/java/com/example/userregister/Users.kt
@@ -0,0 +1,8 @@
+package com.example.userregister
+
+data class Users ( val userId : String = "",
+                   val userName : String = "",
+                   val userAge : Int = 0,
+                   val userEmail : String = ""){
+
+}
