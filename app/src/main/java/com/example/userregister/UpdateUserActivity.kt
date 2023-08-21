7e85e69ce3ad0323c9afcae3517f2a98d84ecdc8
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-08-21 16:03:47 -0300

    Update user feature implemented

--- app/src/main/java/com/example/userregister/UpdateUserActivity.kt
+++ app/src/main/java/com/example/userregister/UpdateUserActivity.kt
@@ -1,16 +1,78 @@
 package com.example.userregister
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.userregister.databinding.ActivityAddUserBinding
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.userregister.databinding.ActivityUpdateUserBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class UpdateUserActivity : AppCompatActivity() {
-    lateinit var updateUserBinding: ActivityUpdateUserBinding
+    private lateinit var updateUserBinding: ActivityUpdateUserBinding
+
+    private val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val myReference : DatabaseReference = database.reference.child("MyUsers")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         updateUserBinding = ActivityUpdateUserBinding.inflate(layoutInflater)
         val view = updateUserBinding.root
         setContentView(view)
+
+        supportActionBar?.title = "Update User"
+        getAndSetData()
+
+        updateUserBinding.buttonUpdateUser.setOnClickListener {
+            updateData()
+        }
+
+    }
+
+    private fun getAndSetData(){
+        val name = intent.getStringExtra("name")
+        val age = intent.getIntExtra("age",0).toString()
+        val email = intent.getStringExtra("email")
+
+        updateUserBinding.editTextUpdateName.setText(name)
+        updateUserBinding.editTextUpdateAge.setText(age)
+        updateUserBinding.editTextUpdateEmail.setText(email)
+    }
+
+    private fun updateData()
+    {
+        val updatedName = updateUserBinding.editTextUpdateName.text.toString()
+        val updatedAge = updateUserBinding.editTextUpdateAge.text.toString().toInt()
+        val updatedEmail = updateUserBinding.editTextUpdateEmail.text.toString()
+
+        val userId = intent.getStringExtra("id").toString()
+
+
+        val userMap = mutableMapOf<String,Any>()
+        userMap["userId"] = userId
+        userMap["userName"] = updatedName
+        userMap["userAge"] = updatedAge
+        userMap["userEmail"] = updatedEmail
+
+        myReference.child(userId).updateChildren(userMap).addOnCompleteListener {
+            task->
+
+            if (task.isSuccessful)
+            {
+                Toast.makeText(applicationContext,
+                "The user has been updated",
+                Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            else
+            {
+                Toast.makeText(applicationContext,
+                    task.exception.toString(),
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+
     }
 }
--- app/src/main/java/com/example/userregister/UsersAdapter.kt
+++ app/src/main/java/com/example/userregister/UsersAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.userregister
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -30,6 +31,22 @@ inner class UsersViewHolder(val adapterBinding : UsersItemBinding)
         holder.adapterBinding.textViewName.text = userList[position].userName
         holder.adapterBinding.textViewAge.text = userList[position].userAge.toString()
         holder.adapterBinding.textViewEmail.text = userList[position].userEmail
+
+        holder.adapterBinding.linearLayout.setOnClickListener {
+            val intent = Intent(context,UpdateUserActivity::class.java)
+            intent.putExtra("id",userList[position].userId)
+            intent.putExtra("name",userList[position].userName)
+            intent.putExtra("age",userList[position].userAge)
+            intent.putExtra("email",userList[position].userEmail)
+
+            context.startActivity(intent)
+        }
+
+
+    }
+
+    fun getUserId(position: Int) : String{
+        return userList[position].userId
     }
 
 }
