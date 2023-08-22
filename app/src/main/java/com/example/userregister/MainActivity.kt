edc284a0f833319f1831646b68dd227c5991d93a
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-08-22 08:33:41 -0300

    Delete all users feature implemented

--- app/src/main/java/com/example/userregister/MainActivity.kt
+++ app/src/main/java/com/example/userregister/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.userregister
 
+import android.annotation.SuppressLint
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.userregister.databinding.ActivityMainBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -31,6 +39,26 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this,AddUserActivity::class.java)
             startActivity(intent)
         }
+
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                  val id = usersAdapter.getUserId(viewHolder.adapterPosition)
+
+                myReference.child(id).removeValue()
+
+                Toast.makeText(applicationContext,"The user has been deleted",Toast.LENGTH_SHORT).show()
+            }
+
+        }).attachToRecyclerView(mainBinding.recyclerView)
+
         retrieveDataFromDatabase()
 
     }
@@ -68,5 +96,48 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_delete_all,menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == R.id.deleteAll)
+        {
+            showDialogMessage()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun showDialogMessage(){
+        val dialogMessage = AlertDialog.Builder(this)
+        dialogMessage.setTitle("Delete All")
+        dialogMessage.setMessage("If Yes is clicked,all users will be deleted, if you want to delete a specific user," +
+                " you can swipe the item you want to delete right or left" )
+        dialogMessage.setNegativeButton("Cancel",DialogInterface.OnClickListener{
+            dialogInterface, i ->
+            dialogInterface.cancel()
+        })
+
+        dialogMessage.setPositiveButton("Yes",DialogInterface.OnClickListener { dialogInterface, i ->
+            myReference.removeValue().addOnCompleteListener { task->
+                if(task.isSuccessful)
+                {
+                    usersAdapter.notifyDataSetChanged()
+
+                    Toast.makeText(applicationContext,"All users were deleted",Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+        dialogMessage.create().show()
+
+
     }
 }
