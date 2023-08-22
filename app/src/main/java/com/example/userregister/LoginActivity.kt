4284a36b10de1aec31ca0efe8564800666841948
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-08-22 13:26:22 -0300

    Sign in feature implemented

--- app/src/main/java/com/example/userregister/LoginActivity.kt
+++ app/src/main/java/com/example/userregister/LoginActivity.kt
@@ -1,11 +1,55 @@
 package com.example.userregister
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.userregister.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginBinding: ActivityLoginBinding
+
+    private val auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        val view  = loginBinding.root
+        setContentView(view)
+
+
+        loginBinding.buttonSignin.setOnClickListener {
+           val userEmail =  loginBinding.editTextEmailSignin.text.toString()
+            val userPassword = loginBinding.editTextPasswordSignin.text.toString()
+
+            signinWithFirebase(userEmail,userPassword)
+        }
+
+
+        loginBinding.buttonSignup.setOnClickListener {
+            val intent  = Intent(this@LoginActivity,SignupActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun signinWithFirebase(userEmail: String, userPassword: String) {
+        auth.signInWithEmailAndPassword(userEmail, userPassword)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(applicationContext,"Login is successful",Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this@LoginActivity,MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(applicationContext,task.exception?.toString(),Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
     }
 }
