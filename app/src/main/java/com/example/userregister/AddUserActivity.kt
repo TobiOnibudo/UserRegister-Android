8587a21ef07de9ad4c53e1ca0b06ce2ae87b405d
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-08-23 17:15:16 -0300

    Image is now uploaded to firebase

--- app/src/main/java/com/example/userregister/AddUserActivity.kt
+++ app/src/main/java/com/example/userregister/AddUserActivity.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
@@ -17,7 +18,9 @@ import com.example.userregister.databinding.ActivityAddUserBinding
 import com.google.android.play.integrity.internal.ac
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Picasso
+import java.util.UUID
 
 
 class AddUserActivity : AppCompatActivity() {
@@ -29,6 +32,10 @@ class AddUserActivity : AppCompatActivity() {
     lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
 
     var imageUri : Uri? = null
+
+    val firebaseStorage : FirebaseStorage = FirebaseStorage.getInstance()
+    val storageReference = firebaseStorage.reference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addUserBinding = ActivityAddUserBinding.inflate(layoutInflater)
@@ -40,7 +47,7 @@ class AddUserActivity : AppCompatActivity() {
 
         supportActionBar?.title = "Add User"
         addUserBinding.buttonAddUser.setOnClickListener {
-            addUserToDatabase()
+           uploadPhoto()
         }
 
         addUserBinding.userProfileImage.setOnClickListener {
@@ -95,7 +102,7 @@ class AddUserActivity : AppCompatActivity() {
 
     }
 
-    fun addUserToDatabase()
+    fun addUserToDatabase(url : String)
     {
         val name : String = addUserBinding.editTextName.text.toString()
         val age : Int = addUserBinding.editTextAge.text.toString().toInt()
@@ -103,7 +110,7 @@ class AddUserActivity : AppCompatActivity() {
 
         val id : String = myReference.push().key.toString()
 
-        val user = Users(id,name,age,email)
+        val user = Users(id,name,age,email,url)
 
         myReference.child(id).setValue(user).addOnCompleteListener {
             task->
@@ -112,7 +119,8 @@ class AddUserActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext,
                     "The new user has been added to the database",
                     Toast.LENGTH_SHORT).show()
-
+                addUserBinding.buttonAddUser.isClickable = true
+                addUserBinding.progressBarAddUser.visibility = View.INVISIBLE
                 finish()
             }
             else
@@ -120,6 +128,35 @@ class AddUserActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext,
                     task.exception.toString(),
                     Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun uploadPhoto(){
+        addUserBinding.buttonAddUser.isClickable = false
+        addUserBinding.progressBarAddUser.visibility = View.VISIBLE
+
+        //UUID
+        val imageName = UUID.randomUUID().toString()
+
+        val imageReference = storageReference.child("images").child(imageName)
+
+        imageUri?.let {
+            uri->
+            imageReference.putFile(uri).addOnSuccessListener {
+                Toast.makeText(applicationContext,"Image uploaded",Toast.LENGTH_SHORT).show()
+
+                    //downloadable url
+                val myUploadedImageReference = storageReference.child("images").child(imageName)
+
+                myUploadedImageReference.downloadUrl.addOnSuccessListener { url->
+                    val imageURL  = url.toString()
+
+                    addUserToDatabase(imageURL)
+                }
+            }.addOnFailureListener{
+                Toast.makeText(applicationContext,it.localizedMessage,Toast.LENGTH_SHORT).show()
+
             }
         }
     }
--- app/src/main/java/com/example/userregister/Users.kt
+++ app/src/main/java/com/example/userregister/Users.kt
@@ -3,6 +3,7 @@ package com.example.userregister
 data class Users ( val userId : String = "",
                    val userName : String = "",
                    val userAge : Int = 0,
-                   val userEmail : String = ""){
+                   val userEmail : String = "",
+                    val url : String = ""){
 
 }
